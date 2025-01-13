5cc29d6bbbd8f9e4199ccc819ea68055add250f4
Xiaodong Wang <44627574+Wang-Xiaodong1899@users.noreply.github.com>
2025-01-13 15:18:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ conda create -n llamafactory python=3.11
 
 conda activate llamafactory
 
-git clone --depth 1 https://github.com/Wang-Xiaodong1899/SkyThought.git
 cd SkyThought/skythought/train/LLaMA-Factory
 
 pip install -e ".[torch,metrics]"
