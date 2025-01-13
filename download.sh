d00ce04f11ea6d71323058a3a42993febb9fc376
Xiaodong Wang <44627574+Wang-Xiaodong1899@users.noreply.github.com>
2025-01-13 14:57:05 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# data
+cd $1
+wget https://hf-mirror.com/datasets/NovaSky-AI/Sky-T1_data_17k/resolve/main/Sky-T1_data_17k.json
+
 base_url="https://hf-mirror.com/Qwen/Qwen2.5-32B-Instruct/resolve/main/"
 
 cd $1
+
+GIT_LFS_SKIP_SMUDGE=1 git clone https://hf-mirror.com/Qwen/Qwen2.5-32B-Instruct
+
+cd Qwen2.5-32B-Instruct
+
+rm model-000*
 
 start=1
 end=17
@@ -22,3 +32,4 @@ for i in $(seq -f "%02g" $start $end); do
 done
 
 echo "Download complete."
+
